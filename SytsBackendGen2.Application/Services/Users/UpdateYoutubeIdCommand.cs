4faab8c440a9cad00d3d7d643408e0cfc15aca5c
Семён Бородяев <meckbaig@yandex.ru>
2024-09-06 20:49:28 +0500

    Add UserController with UpdateYoutubeId endpoint

---
+++ SytsBackendGen2.Application/Services/Users/UpdateYoutubeIdCommand.cs
@@ -0,0 +1,61 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using SytsBackendGen2.Application.Common.BaseRequests;
+using SytsBackendGen2.Application.Common.Exceptions;
+using SytsBackendGen2.Application.Common.Interfaces;
+using SytsBackendGen2.Domain.Entities.Authentification;
+
+namespace SytsBackendGen2.Application.Services.Users;
+
+public record UpdateYoutubeIdCommand : BaseRequest<UpdateYoutubeIdResponse>
+{
+    public string youtubeId { get; set; }
+    public ClaimsPrincipal principal { get; set; }
+}
+
+public class UpdateYoutubeIdResponse : BaseResponse
+{
+
+}
+
+public class UpdateYoutubeIdCommandValidator : AbstractValidator<UpdateYoutubeIdCommand>
+{
+    public UpdateYoutubeIdCommandValidator()
+    {
+        RuleFor(x => x.youtubeId).Length(24);
+    }
+}
+
+public class UpdateYoutubeIdCommandHandler : IRequestHandler<UpdateYoutubeIdCommand, UpdateYoutubeIdResponse>
+{
+    private readonly IAppDbContext _context;
+    private readonly IJwtProvider _jwtProvider;
+
+    public UpdateYoutubeIdCommandHandler(IAppDbContext context, IJwtProvider jwtProvider)
+    {
+        _context = context;
+        _jwtProvider = jwtProvider;
+    }
+
+    public async Task<UpdateYoutubeIdResponse> Handle(UpdateYoutubeIdCommand request, CancellationToken cancellationToken)
+    {
+        int userId = _jwtProvider.GetUserIdFromClaimsPrincipal(request.principal);
+
+        User? user = await _context.Users
+            .FirstOrDefaultAsync(x => x.Id == userId, cancellationToken);
+
+        if (user == null)
+        {
+            throw new Common.Exceptions.ValidationException(
+                "JWT token",
+                [new ErrorItem($"Unable to find user with id {userId}.", ValidationErrorCode.EntityIdValidator)]);
+        }
+
+        user.YoutubeId = request.youtubeId;
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new UpdateYoutubeIdResponse();
+    }
+}
---
+++ SytsBackendGen2.Web/Controllers/V1/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using SytsBackendGen2.Application.Services.Users;
+using SytsBackendGen2.Domain.Enums;
+using SytsBackendGen2.Infrastructure.Authentification.Permissions;
+
+namespace SytsBackendGen2.Web.Controllers.V1;
+
+[Route("api/v{version:ApiVersion}/[controller]")]
+[ApiController]
+[ApiVersion("1")]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    [HasPermission(Permission.PrivateDataEditor)]
+    [Route("UpdateYoutubeId")]
+    public async Task<ActionResult<UpdateYoutubeIdResponse>> UpdateYoutubeId([FromBody] UpdateYoutubeId body)
+    {
+        var command = new UpdateYoutubeIdCommand { youtubeId = body.youtubeId, principal = User };
+        var result = await _mediator.Send(command);
+        return result.ToJsonResponse();
+    }
+}
+
+public sealed record UpdateYoutubeId(string youtubeId);

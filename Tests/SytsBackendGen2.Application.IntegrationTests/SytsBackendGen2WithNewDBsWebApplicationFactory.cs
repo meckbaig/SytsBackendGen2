62d1d508498c75148d4b7d06068c5dda32fd837a
Семён Бородяев <meckbaig@yandex.ru>
2024-09-14 14:44:03 +0500

    Add IGoogleAuthProvider mocking in SytsBackendGen2WithNewDBsWebApplicationFactory

--- Tests/SytsBackendGen2.Application.IntegrationTests/SytsBackendGen2WithNewDBsWebApplicationFactory.cs
+++ Tests/SytsBackendGen2.Application.IntegrationTests/SytsBackendGen2WithNewDBsWebApplicationFactory.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System.Data.Common;
+using SytsBackendGen2.Application.Common.Interfaces;
+using SytsBackendGen2.Application.DTOs.Folders;
+using SytsBackendGen2.Application.DTOs.Users;
 using SytsBackendGen2.Infrastructure.Data;
 using Testcontainers.PostgreSql;
 
@@ -50,6 +53,12 @@ namespace SytsBackendGen2.Application.SystemTests.Controllers
                     return _connection;
                 });
 
+                // Remove the real IGoogleAuthProvider and add the mock
+                var descriptorGoogle = services.SingleOrDefault(d => d.ServiceType == typeof(IGoogleAuthProvider));
+                if (descriptorGoogle != null)
+                    services.Remove(descriptorGoogle);
+                services.AddSingleton<IGoogleAuthProvider, MockGoogleAuthProvider>();
+
                 // Seed the database after it's created
                 using (var scope = services.BuildServiceProvider().CreateScope())
                 {
@@ -90,5 +99,80 @@ namespace SytsBackendGen2.Application.SystemTests.Controllers
             }
             base.Dispose(disposing);
         }
+    }
+}
+
+public class MockGoogleAuthProvider : IGoogleAuthProvider
+{
+    private bool _multiplePages = false;
+    private int _pageCount = 0;
+    private string _invalidAccessToken = null;
+    private string _newUserEmail = null;
+
+    public Task<UserPreviewDto?> AuthorizeAsync(string accessToken)
+    {
+        if (accessToken == _invalidAccessToken)
+        {
+            throw new HttpRequestException("Invalid access token");
+        }
+
+        var userPreview = new UserPreviewDto(
+            "Test User", 
+            _newUserEmail ?? "first@gmail.com", 
+            "https://example.com/profile.jpg"
+        );
+        return Task.FromResult<UserPreviewDto?>(userPreview);
+    }
+
+    public Task<string> GetYoutubeIdByName(string username)
+    {
+        return Task.FromResult("UCOByaobXOl6YYxUNBxSwe_7");
+    }
+
+    public Task<(List<SubChannelDto>, string?)> GetSubChannels(string channelId, string? nextPageToken = null)
+    {
+        if (!_multiplePages)
+        {
+            var subChannel = new SubChannelDto
+            {
+                Title = "Test Channel",
+                ThumbnailUrl = "https://example.com/thumbnail.jpg",
+                ChannelId = "UC1234567890"
+            };
+
+            return Task.FromResult((new List<SubChannelDto> { subChannel }, (string?)null));
+        }
+        else
+        {
+            _pageCount++;
+            var subChannels = new List<SubChannelDto>
+            {
+                new SubChannelDto
+                {
+                    Title = $"Test Channel {_pageCount}",
+                    ThumbnailUrl = $"https://example.com/thumbnail{_pageCount}.jpg",
+                    ChannelId = $"UC123456789{_pageCount}"
+                }
+            };
+
+            string? nextToken = _pageCount < 3 ? "next_page_token" : null;
+            return Task.FromResult((subChannels, nextToken));
+        }
+    }
+
+    public void SetMultiplePages(bool value)
+    {
+        _multiplePages = value;
+        _pageCount = 0;
+    }
+
+    public void SetInvalidAccessToken(string invalidToken)
+    {
+        _invalidAccessToken = invalidToken;
+    }
+
+    public void SetNewUser(string email)
+    {
+        _newUserEmail = email;
     }
 }
